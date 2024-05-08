861ce949f67fcf30a0400441e89f4e6d23d7081b
HoppeMariel <mariel.hoppe@gmail.com>
2024-05-08 20:58:13 -0300

    modifico el orden de los prompt plan y cantidadMascotas

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-function simuladorPlan(cantidadMascotas, plan) {
+
+function simularPlan(cantidadMascotas, plan) {
 
   const descuento=0.8
 
@@ -16,7 +17,7 @@ function simuladorPlan(cantidadMascotas, plan) {
         console.log("Ingresá un plan válido.");
   }
 
-  console.log("Elegiste el plan " + plan + ".El precio del plan individual es $" + precioPlan)
+  console.log("Elegiste el plan ", plan , ".El precio del plan individual es $", precioPlan)
   
   for (let i = 1; i <= 12; i++) {
     if (cantidadMascotas >= 3) {
@@ -30,7 +31,9 @@ function simuladorPlan(cantidadMascotas, plan) {
   }
 }
 
-let cantidadMascotas = parseInt(prompt("Ingrese la cantidad de mascotas:"));
-let plan = prompt("Ingrese el plan (Pardo, Polar o Panda):");
+let plan = prompt("Ingresá el plan que querés consultar (Pardo, Polar o Panda):");
 
-simuladorPlan(cantidadMascotas, plan);
+let cantidadMascotas = parseInt(prompt("Ingresá la cantidad de mascotas que tenés "));
+
+simularPlan(cantidadMascotas, plan);
+
